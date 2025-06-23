f14ee54cd3e6162e18ef0429ee7c5b57184d1234
Ryan Viajedor <ryanmoradasviajedor@gmail.com>
2025-06-23 21:42:40 +0300

    Initial setup window without Main

---
+++ CardWorkout-Programmatic/CardSelectionVC.swift
@@ -0,0 +1,18 @@
+//
+//  CardSelectionVC.swift
+//  CardWorkout-Programmatic
+//
+//  Created by Ryan Viajedor on 6/23/25.
+//
+
+import UIKit
+
+class CardSelectionVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .purple
+    }
+
+}
