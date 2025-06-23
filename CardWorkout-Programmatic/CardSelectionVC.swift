4dade8b5f94caf023a64cdb544c83201dd02209b
Ryan Viajedor <ryanmoradasviajedor@gmail.com>
2025-06-23 22:30:01 +0300

    configure Card Image View

--- CardWorkout-Programmatic/CardSelectionVC.swift
+++ CardWorkout-Programmatic/CardSelectionVC.swift
@@ -18,12 +18,25 @@ class CardSelectionVC: UIViewController {
         super.viewDidLoad()
 
         view.backgroundColor = .systemBackground
+        configureUI()
     }
     
     func configureUI () {
+        configureCardImageView()
     }
     
     func configureCardImageView () {
+        view.addSubview(cardImageView)
+        cardImageView.translatesAutoresizingMaskIntoConstraints = false
+        cardImageView.image = UIImage(named: "AS")
+        
+        NSLayoutConstraint.activate([
+            cardImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            cardImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -75),
+            cardImageView.widthAnchor.constraint(equalToConstant: 250),
+            cardImageView.heightAnchor.constraint(equalToConstant: 350)
+        ])
     }
+    
 
 }
